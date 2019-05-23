5d93540de8386d57012e56952a1db2f9cfcbc3af
Gabor <orbangabeszb@gmail.com>
2019-05-23 21:22:02 +0200

    replace method used

--- src/MapProgram.java
+++ src/MapProgram.java
@@ -24,6 +24,22 @@ public class MapProgram {
 		
 		System.out.println("============================================");
 		
+//		languages.remove("Lisp"); //remove() also returns a boolean
+		if(languages.remove("Algol", "a family of algorithmic languages")) {
+			System.out.println("Algol removed");
+		} else {
+			System.out.println("Algol not removed, key/value pair not found");
+		}
+		
+		//replace(key, old value, new value), else returns false
+		if(languages.replace("Lisp", "Therein lies madness", "a functional programming language with imperative features")) {
+			System.out.println("Lisp replaced");
+		} else {
+			System.out.println("Lisp was not replaced");
+		}
+
+//		System.out.println(languages.replace("Scala", "this will not be added"));
+		
 		for(String key : languages.keySet()) { //goes through all keys, more in Sets section, doesn'T return them in the correct order wit hash map
 			System.out.println(key + " : " + languages.get(key));
 		}
