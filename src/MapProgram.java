b1a1233e26c1ca2652481d63b8699ed60e768d7b
Gabor <orbangabeszb@gmail.com>
2019-05-23 20:57:33 +0200

    containsKey check applied

--- src/MapProgram.java
+++ src/MapProgram.java
@@ -5,13 +5,28 @@ public class MapProgram {
 
 	public static void main(String[] args) {
 		Map<String, String> languages = new HashMap<>();
-		languages.put("Java", "a compiled, high-level, object-oriented, platform-independent language"); //put~add, needs key and definition
+		languages.put("Java", "a compiled, high-level, object-oriented, platform-independent language"); //put~add, needs key and value
 		languages.put("Python", "an interpreted, object-orinted, high-level programming language with dynamic semantics");
 		languages.put("Algol", "an algorithmic language");
-		languages.put("BASIC", "Beginners All Purposes Instruction Code");
-		languages.put("Lisp", "Therein lies madness");
+		System.out.println(languages.put("BASIC", "Beginners All Purposes Instruction Code"));
+		System.out.println(languages.put("Lisp", "Therein lies madness"));
 		
-		System.out.println(languages.get("Java"));
+//		System.out.println(languages.put("Java", "this course is about Java")); //overwrites value, displays the overwritten value at this moment
+//		System.out.println(languages.get("Java")); //displays the new value of the key
+		
+		
+		//use the containsKey method to check and not overwrite the value of the key
+		if(languages.containsKey("Java")) {
+			System.out.println("Java is already in the map");
+		} else {
+			languages.put("Java", "this course is about Java");
+		}
+		
+		System.out.println("============================================");
+		
+		for(String key : languages.keySet()) { //goes through all keys, more in Sets section, doesn'T return them in the correct order wit hash map
+			System.out.println(key + " : " + languages.get(key));
+		}
 		
 	}
 }
