83112d0c98e22c0ef8bb27631cf6e05f7508e423
Gabor <orbangabeszb@gmail.com>
2019-05-23 16:45:44 +0200

    bubble search method applied as example

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -16,6 +17,16 @@ public class Main {
 			System.out.println("Seat already reserved");
 		}
 
+		Collections.shuffle(seatCopy); //can also try .reverse
+		System.out.println("Printing seatCopy");
+		printList(seatCopy);
+		System.out.println("Printing theatre.seats");
+		printList(theatre.seats);
+		
+		Theatre.Seat minSeat = Collections.min(seatCopy);
+		Theatre.Seat maxSeat = Collections.max(seatCopy);
+		System.out.println("Min seat number is " + minSeat.getSeatNumber());
+		System.out.println("Max seat number is " + maxSeat.getSeatNumber());
 	}
 	
 	public static void printList(List<Theatre.Seat> list) { //shallow copy, refers to the same objects
@@ -24,6 +35,16 @@ public class Main {
 		}
 		System.out.println();
 		System.out.println("========================================================");
+	}
+	
+	public static void sortList(List<? extends Theatre.Seat> list) { //bubble search
+		for(int i=0; i<list.size(); i++) {
+			for(int j=i+1; j<list.size(); j++) { //j=i+1 means with each loop it searches through less
+				if(list.get(i).compareTo(list.get(j)) > 0) {
+					Collections.swap(list, i, j); //add the list and the elements we want to swap
+				}
+			}
+		}
 	}
 
 }
